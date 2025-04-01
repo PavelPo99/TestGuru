5885bb61f93d1980afad062b0e067a65f8632b94
PavelPo99 <pasha_alexeev99@mail.ru>
2025-04-01 21:21:02 +0300

    fixed i18n in form_inline.js

--- app/javascript/utilities/form_inline.js
+++ app/javascript/utilities/form_inline.js
@@ -1,56 +1,26 @@
 document.addEventListener('turbo:load', function() {
-  // var controls = document.querySelectorAll('.form-inline-link')
-
-  // if (controls.length) {
-  //   for (var i = 0; i < controls.length; i++ ) {
-  //     controls[i].addEventListener('click', formInlineLinkHandler)
-  //   }
-  // }
-
   $('.form-inline-link').on('click', formInlineLinkHandler)
-
-//   var errors = document.querySelector('.resource-errors')
-
-//   if (errors) {
-//     var resourceId = this.dataset.resourceId
-//     formInlineHandler(resourceId)
-//   }
 })
 
 function formInlineLinkHandler(event) {
   event.preventDefault()
 
-  var testId = this.dataset.testId
-  formInlineHandler(testId)
+  const testId = this.dataset.testId
+  const cancelText = this.dataset.cancel;
+  const editText = this.dataset.edit;
+
+  formInlineHandler(testId, cancelText, editText)  
 }
 
-function formInlineHandler(testId) {
-  var link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]')
-  // var testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"]')
-  // var formInline = document.querySelector('.from-inline[data-test-id="' + testId + '"]')
+function formInlineHandler(testId, cancelText, editText) {
+  const link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]')
 
-  var $testTitle = $('.test-title[data-test-id="' + testId + '"]')
-  var $formInline = $('.from-inline[data-test-id="' + testId + '"]')
+  let $testTitle = $('.test-title[data-test-id="' + testId + '"]')
+  let $formInline = $('.from-inline[data-test-id="' + testId + '"]')
 
 
   $testTitle.toggle()
   $formInline.toggle()
 
-  if ($formInline.is(':visible')) {
-    link.textContent = document.querySelector('.nav-link.active').textContent.trim() === 'Рус' ?  "Отмена" : "Cancel"
-  } else {
-    link.textContent = document.querySelector('.nav-link.active').textContent.trim() === 'Рус' ?  "Редактировать" : "Edit"
-  }
-
-
-
-  // if (formInline.classList.contains('hide')) { 
-  //   testTitle.classList.add('hide')
-  //   formInline.classList.remove('hide')
-  //   link.textContent = 'Cancel'
-  // } else {
-  //   testTitle.classList.remove('hide')
-  //   formInline.classList.add('hide')
-  //   link.textContent = 'Edit'
-  // }
+  link.textContent = $formInline.is(':visible') ? cancelText : editText;
 }
