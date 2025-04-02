da6c0d9658941b8956b992cd6736cb0ac236c77d
PavelPo99 <pasha_alexeev99@mail.ru>
2025-04-02 22:13:46 +0300

    delete link_tree in manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,6 @@
-//= link_tree ../images
 //= link_tree ../builds
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_directory ../stylesheets .scss
 //= link application.css
-// = link jquery.min.js
+//= link jquery.min.js
