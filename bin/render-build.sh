690793441a09637e79ea59c8095e8a46a429f373
PavelPo99 <pasha_alexeev99@mail.ru>
2025-04-14 23:42:54 +0300

    fixed mistake render-build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,6 @@ set -o errexit
 
 bundle install
 yarn install
-bundler exec rake assets:precompile
-bundler exec rake assets:clean
-bundler exec rake db:migrate
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
