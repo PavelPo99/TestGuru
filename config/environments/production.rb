39e2b87cb272146c7bfd63d1b9336b4c487d71c4
PavelPo99 <pasha_alexeev99@mail.ru>
2025-04-14 22:14:36 +0300

    preparing for deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   # config.require_master_key = true
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
-  # config.public_file_server.enabled = false
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present? || ENV["RENDER"].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -77,6 +77,19 @@ Rails.application.configure do
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
   config.action_mailer.perform_caching = false
+  # config.action_mailer.default_url_options = { host: }# 'test-guru-2bt5.onrender.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.mail.ru",
+    port:                 465,
+    domain:               "mail.ru",
+    authentication:       "plain",
+    user_name:            ENV["SMTP_USERNAME"],
+    password:             ENV["SMTP_PASSWORD"],
+    ssl:                  true,
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
