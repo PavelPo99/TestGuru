a9f70753f54994abfbf928a4620878a11ff331ab
PavelPo99 <pasha_alexeev99@mail.ru>
2025-04-16 21:58:35 +0300

    setting up mail #2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              "smtp.mail.ru",
     port:                 465,
-    domain:               "mail.ru",
     authentication:       "plain",
     user_name:            ENV["SMTP_USERNAME"],
     password:             ENV["SMTP_PASSWORD"],
