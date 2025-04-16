44e91232eaaa2689d6016e3fc71b885475ff7495
PavelPo99 <pasha_alexeev99@mail.ru>
2025-04-16 21:57:28 +0300

    setting up mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.logger = Logger.new($stdout)
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
@@ -92,7 +93,8 @@ Rails.application.configure do
     password:             ENV["SMTP_PASSWORD"],
     ssl:                  true,
     tls:                  true,
-    enable_starttls_auto: true
+    enable_starttls_auto: true,
+    openssl_verify_mode: "none"
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
