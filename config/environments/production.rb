aae9fe6caa0a6fa2af0e6be50303e5ff12e5c011
PavelPo99 <pasha_alexeev99@mail.ru>
2025-04-16 22:13:54 +0300

    setting up mail #3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,15 +85,14 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              "smtp.mail.ru",
+    address:              "smtp.yandex.ru",
     port:                 465,
+    domain:               "yandex.ru",
     authentication:       "plain",
     user_name:            ENV["SMTP_USERNAME"],
     password:             ENV["SMTP_PASSWORD"],
     ssl:                  true,
-    tls:                  true,
-    enable_starttls_auto: true,
-    openssl_verify_mode: "none"
+    enable_starttls_auto: true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
